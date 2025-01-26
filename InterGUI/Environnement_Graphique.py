2356bc2a8cd9022b6f9d047801d8faa8362af469
DomWarrior <101674536+DomWarrior@users.noreply.github.com>
2025-01-26 16:08:04 -0500

    modif2

--- InterGUI/Environnement_Graphique.py
+++ InterGUI/Environnement_Graphique.py
@@ -1,1 +1,17 @@
-import tkinter
+import tkinter as tk
+import BouletDeCanon as BC
+
+
+root = tk.Tk()
+root.title("Capitaine Morgan")
+
+def on_click():
+    BC.equProjectile(45,9.81,15,3)
+
+lbl= tk.Label(root, text='Test1')
+lbl.grid(row=0,column=0)
+
+bouton=tk.Button(root, text='FEU!', command=on_click)
+bouton.grid()
+
+root.mainloop()
